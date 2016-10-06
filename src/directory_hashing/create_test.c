0cf92bf0b7cc674761310a29d1414ae347b35dd6
DragonCannon <9cpaddy@gmail.com>
2016-10-06 23:26:44 +1300

    create a C program that generates a large number of files and populates them with random data. Files used to test directory hasing approach vs normal file search

---
+++ src/directory_hashing/create_test.c
@@ -0,0 +1,105 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include <string.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <errno.h>
+
+char randomChar() {
+    int randomIndex;
+    char letters[64] = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's',
+                        't', 'u', 'v', 'w', 'x', 'y', 'z',
+                        'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S',
+                        'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '-', '_'};
+
+    randomIndex = randomArrayInt(sizeof(letters));
+    return letters[randomIndex];
+}
+
+int randomArrayInt(int max) {
+    int r;
+    r = rand() % max;
+    return r;
+}
+
+int randomInt(int min, int max) {
+    int r = 0;
+
+    while (r < min) {
+        r = rand() % max + 1;
+    }
+    return r;
+}
+
+int main(int argc, char *argv) {
+    int dir1, dir2, i, j, filenameLength, len, res, writeAmount, k;
+    unsigned int hash;
+    char *c, *tmp, newChar, *filename, buffer[3];
+    FILE *fp;
+    struct stat st = {0};
+
+    c = calloc(1, sizeof(char));
+    tmp = calloc(1, sizeof(char));
+    filename = calloc(1, sizeof(char));
+    srand(time(NULL));
+
+    i = 0;
+    j = 0;
+    filenameLength = 0;
+
+    for (i = 0; i < 1000; i = i + 1) {
+        filenameLength = randomInt(5, 20);
+        for (j = 0; j < filenameLength; j = j + 1) {
+            len = strlen(c);
+
+            tmp = realloc(tmp, len + 1);
+            strcpy(tmp, c);
+
+            newChar = randomChar();
+
+            c = realloc(c, len + 2);
+            strcpy(c, tmp);
+            c[len] = newChar;
+            c[len + 1] = '\0';
+        }
+
+        hash = fnv(c);
+        dir1 = hash & 255;
+        hash = hash >> 8;
+        dir2 = hash & 255;
+
+        filename = realloc(filename, 10 + strlen(c));
+        sprintf(buffer, "%d", dir1);
+        strcpy(filename, buffer);
+
+        if (stat(filename, &st) == -1) {
+            mkdir(filename, 0777);
+        }
+
+        strcat(filename, "/");
+        sprintf(buffer, "%d", dir2);
+        strcat(filename, buffer);
+
+        if (stat(filename, &st) == -1) {
+            mkdir(filename, 0777);
+        }
+
+        strcat(filename, "/");
+        strcat(filename, c);
+
+        writeAmount = randomInt(1000, 10000);
+        for (k = 0; k < writeAmount; k = k + 1) {
+            fprintf(fp, "%c", randomChar());
+        }
+
+        memset(c, 0, len);
+        memset(tmp, 0, len);
+        memset(filename, 0, strlen(filename));
+    }
+
+    free(c);
+    free(tmp);
+    free(filename);
+}
